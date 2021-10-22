25ef880edb4610b666caa79eadfd00960f4e93f9
Adeleke Ayinde <lekea4@gmail.com>
2021-10-22 23:46:21 +0100

    Modify the Producer class to fix typo in parameters and create a migration file to effect changes to the database

---
+++ eTickets/Migrations/20211022222056_fixProducerTypo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eTickets.Migrations
+{
+    public partial class fixProducerTypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureUrl",
+                table: "Producers",
+                newName: "ProfilePictureURL");
+
+            migrationBuilder.RenameColumn(
+                name: "FullNmae",
+                table: "Producers",
+                newName: "FullName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProfilePictureURL",
+                table: "Producers",
+                newName: "ProfilePictureUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Producers",
+                newName: "FullNmae");
+        }
+    }
+}
