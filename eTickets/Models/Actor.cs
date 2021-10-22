f2c8030ac357eaa89de2f0520efc78b12028de1d
Adeleke Ayinde <lekea4@gmail.com>
2021-10-22 21:30:16 +0100

    Implement the actor class

---
+++ eTickets/Models/Actor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string ProfilePictureUrl { get; set; }
+
+        public string FullNmae { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
