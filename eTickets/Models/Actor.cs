56426864d2aedea4230ecb0e46d86cb3ba9e0b4c
Adeleke Ayinde <lekea4@gmail.com>
2021-11-05 18:36:01 +0100

    Modify the create and details files in the Actor folder

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -18,7 +18,9 @@ namespace eTickets.Models
 
         [Display(Name = "Full Name")]
         [Required (ErrorMessage ="Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
+
 
         [Display(Name = "Biography")]
         [Required (ErrorMessage ="Biography is required")]
