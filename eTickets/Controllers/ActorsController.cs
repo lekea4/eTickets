e9cdad613a6e49236925e1587d46fd88b9e5ce0f
Adeleke Ayinde <lekea4@gmail.com>
2021-11-05 18:32:37 +0100

    Modify the Actor controller to include the delete action

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -48,13 +48,66 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Details(int id)
         {
-            var actorsDetails = await _service.GetByIdAsync(id); 
+            var actorDetails = await _service.GetByIdAsync(id); 
 
-            if (actorsDetails == null)
-                return View("Empty");
+            if (actorDetails == null)
+                return View("NotFound");
 
 
-            return View(actorsDetails);
+            return View(actorDetails);
+        }
+
+
+
+        //GET: Actors/Edit
+        public async Task <IActionResult> Edit(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null)
+                return View("NotFound");
+
+
+            return View(actorDetails);
+           
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FullName,ProfilePictureURL,Bio")] Actor actor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(actor);
+            }
+
+            await _service.UpdateAsync(id, actor);
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        //GET: Actors/Delete
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null)
+                return View("NotFound");
+
+
+            return View(actorDetails);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+
+            if (actorDetails == null) return View("NotFound");
+            await _service.DeleteAsync(id);
+
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
